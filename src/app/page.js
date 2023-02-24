f3dd60d89191b68a62894a203bca97b4a65a862a
wpr-25 <willy.ok1025@gmail.com>
2023-02-24 15:27:40 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -18,8 +18,8 @@ export default function Home() {
             </div>
             <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
               <div class="p-8 sm:p-10 lg:flex-auto">
-                <h3 class="text-2xl font-bold tracking-tight text-gray-900">Dapatkan Banyak Promo</h3>
-                <p class="mt-6 text-lg leading-7 text-black">hanya dengan berbelanja di toko merah kuning hijau biru dan ungu, kamu akan mendapatkan banyak promo menarik!!.</p>
+                <h3 class="text-2xl font-bold tracking-tight text-gray-900">KUY GOX</h3>
+                <p class="mt-6 text-lg leading-7 text-black">KUY GOX “Go With The Box”, merupakan produk terbaru dari MOVI yang memiliki bentuk yang menarik dan beda dari pod kotak pada umumnya. Bentuknya memiliki istilah Double Barrel Shape dan jika dilihat dari atas, akan terlihat seperti angka 8. Dengan bentuk tersebut, KUY GOX ini terlihat compact dan kokoh saat diposisikan berdiri serta sangat nyaman saat digenggam dengan berbagai cara.</p>
                 <div class="mt-10 flex items-center gap-x-4">
                   <h4 class="flex-none text-sm font-semibold leading-6 text-black">Apa yang kamu dapatkan?</h4>
                   <div class="h-px flex-auto bg-gray-100"></div>
@@ -29,40 +29,40 @@ export default function Home() {
                     <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                       <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
                     </svg>
-                    Rasa Apel Hijau yang segar
+                    Kapasitas baterai: 950 mAh
                   </li>
 
                   <li class="flex gap-x-3">
                     <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                       <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
                     </svg>
-                    Rasa Dingin yang tidak menyakitkan
+                    Waktu inhale terpanjang: 6 detik
                   </li>
 
                   <li class="flex gap-x-3">
                     <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                       <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
                     </svg>
-                    Nikmat tanpa sedak
+                    Kapasitas liquid: 1.8ml
                   </li>
 
                   <li class="flex gap-x-3">
                     <svg class="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                       <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
                     </svg>
-                    Lembut seperti Kapas
+                    Ketahanan coil: 0.8 ohm
                   </li>
                 </ul>
               </div>
               <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
                 <div class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
                   <div class="mx-auto max-w-xs px-8">
-                    <p class="text-base font-semibold text-gray-600">AFLO Glacier Apple</p>
+                    <p class="text-base font-semibold text-gray-600">KUY GOX</p>
                     <p class="mt-6 flex items-baseline justify-center gap-x-2">
-                      <span class="text-5xl font-bold tracking-tight text-gray-900"><img src="https://movi.id/wp-content/uploads/2022/05/b0901bef-c857-41a6-a504-ed4589120ab8.jpg" className="rounded-md" alt="Liquid Aflo Terbaik"/></span>
+                      <span class="text-5xl font-bold tracking-tight text-gray-900"><img src="https://movi.id/wp-content/uploads/2023/01/Kuy_Gox_1-1024x384.jpg" className="rounded-md" alt="Kuy Gox"/></span>
 
                     </p>
-                    <Link href="#" class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">BELI</Link>
+                    <Link href="https://www.tokopedia.com/ministryofvapeid/kuy-gox-by-movi-auric-gold?extParam=whid%3D60056" class="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">BELI</Link>
                     <p class="mt-6 text-xs leading-5 text-gray-600">Produk Terbatas !!</p>
                   </div>
                 </div>
@@ -78,15 +78,15 @@ export default function Home() {
           <div class="mx-auto max-w-7xl py-24 sm:px-6 lg:px-8">
             <div class="relative isolate overflow-hidden bg-gradient-to-tl from-sky-300 via-sky-200 to-blue-600 px-6 pt-16 hover:shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
               <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-                <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Best Product For<br />AFLO Liquid.</h2>
-                <p class="mt-6 text-lg leading-8 text-black">Aflo.id by Ministry Of Vape Indonesia.</p>
+                <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Best Device For<br />KUY GOX.</h2>
+                <p class="mt-6 text-lg leading-8 text-black">KUY GOX - Kuy By MOVI.</p>
                 <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                  <Link href="https://www.tokopedia.com/ministryofvapeid/etalase/liquid-aflo-by-movi" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-2xl hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Click To View</Link>
-                  <Link href="https://movi.id/id/liquid-aflo-mint-series/" class="text-sm font-semibold leading-6 text-black">Learn more <span aria-hidden="true">→</span></Link>
+                  <Link href="https://www.tokopedia.com/ministryofvapeid/cartridge-kuy-gox-by-movi-freshy-lilac?extParam=whid%3D60056" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-2xl hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Click To View</Link>
+                  <Link href="https://movi.id/en/kuy-gox-dari-movi/" class="text-sm font-semibold leading-6 text-black">Learn more <span aria-hidden="true">→</span></Link>
                 </div>
               </div>
               <div class="relative mt-16 h-80 lg:mt-8">
-                <img class="absolute opacity-70 py-8 top-0 left-0 w-[26rem] max-w-none rounded-lg bg-white/5 ring-1 ring-white/10 shadow-2xl" src="https://movi.id/wp-content/uploads/2022/10/K-80.jpg" alt="App Liquid Dingin" width="1824" height="1080" />
+                <img class=" opacity-70 ml-20 mt-16 max-w-none rounded-lg bg-white/5 ring-1 ring-white/10 shadow-2xl items-center place-content-center" src="https://movi.id/wp-content/uploads/2023/01/Kuy_Gox_5.jpg" alt="KUY GOX" width="250" height="250" />
               </div>
             </div>
           </div>
